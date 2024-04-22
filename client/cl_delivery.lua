c7dbe26dd20479d306d366b80bc1267593f079a7
Benji <scrupab@gmail.com>
2024-04-22 08:11:28 -0400

    Update cl_delivery.lua

--- client/cl_delivery.lua
+++ client/cl_delivery.lua
@@ -238,16 +238,15 @@ function StartDeliveryJob(ped, sNum)
     SetPedIntoVehicle(ped, dTruck, -1)
     if Config.Framework == 'qb' then
         TriggerServerEvent('qb-vehiclekeys:server:GiveVehicleKeys', GetVehicleNumberPlateText(dTruck))
-        elseif Config.Framework == 'esx' then
-            SetVehicleDoorsLocked(dTruck, 1)
-        elseif Config.Framework == 'nd' then
-            SetVehicleDoorsLocked(dTruck, 1)
-        elseif Config.Framework == 'qbx' then
-            TriggerServerEvent('qb-vehiclekeys:server:GiveVehicleKeys', -1, GetVehicleNumberPlateText(dTruck))
-        else
-            SetVehicleDoorsLocked(dTruck, 1)
-            print("^5Debug^7: ^1You dont have a framework selected for keys")
-        end
+    elseif Config.Framework == 'esx' then
+        SetVehicleDoorsLocked(dTruck, 1)
+    elseif Config.Framework == 'nd' then
+        SetVehicleDoorsLocked(dTruck, 1)
+    elseif Config.Framework == 'qbx' then
+        TriggerServerEvent('qb-vehiclekeys:server:GiveVehicleKeys', -1, GetVehicleNumberPlateText(dTruck))
+    else
+        SetVehicleDoorsLocked(dTruck, 1)
+        print("^5Debug^7: ^1You dont have a framework selected for keys")
     end
 
     isOnDeliveryDuty = true
